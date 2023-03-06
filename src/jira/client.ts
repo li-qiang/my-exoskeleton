2dea24da15512d8d4e77db0cb9c1f9a68da33954
zchengb <xiaobin.zheng@thoughtworks.com>
2023-03-06 23:21:40 +0800

    [xiaobin] feat: change to search issues by current user.

--- src/jira/client.ts
+++ src/jira/client.ts
@@ -4,16 +4,15 @@ import request from "./request";
 
 const {otrJiraToken} = getPreferenceValues<JiraPreference>();
 
-    const fetchIssuesByCurrentUser = () : Promise<JiraIssues> => {
-        return request<JiraIssues>({
-            // url: '/api/2/search?jql=reporter+%3D+currentUser()+order+by+created+DESC',
-            url: '/api/2/search',
-            method: 'get',
-            headers: {...{"Authorization": `Bearer ${otrJiraToken}`}},
-        }).then(res => {
-            return res.data
-        });
-    };
+const fetchIssuesByCurrentUser = (): Promise<JiraIssues> => {
+    return request<JiraIssues>({
+        url: '/api/2/search?jql=reporter+%3D+currentUser()+order+by+created+DESC',
+        method: 'get',
+        headers: {...{"Authorization": `Bearer ${otrJiraToken}`}},
+    }).then(res => {
+        return res.data
+    });
+};
 
 export const JiraClient = {
     fetchIssuesByCurrentUser,
