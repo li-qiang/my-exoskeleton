f9e2dc2f738d985daf3caeefb2d192c6e467d51e
gejian <gejian.liao@thoughtworks.com>
2023-03-06 15:49:57 +0800

    fix: fix token parse

--- src/bookmark/JWTClient.ts
+++ src/bookmark/JWTClient.ts
@@ -4,16 +4,21 @@ import {BookmarkPreference} from "./types";
 
 const {BookmarkServiceAccountKey} = getPreferenceValues<BookmarkPreference>()
 
-// const keys = JSON.parse(BookmarkServiceAccountKey);
-const keys = {
-    client_email: '',
-    private_key: ''
+
+const getJwt = () => {
+    try {
+        const keys = JSON.parse(BookmarkServiceAccountKey);
+        return new JWT({
+            email: keys.client_email,
+            key: keys.private_key,
+            scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+        });
+    }catch (e) {
+        return null
+    }
 }
-const jwtclient = new JWT({
-    email: keys.client_email,
-    key: keys.private_key,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-});
+
+const jwtclient = getJwt()
 
 export {
     jwtclient
